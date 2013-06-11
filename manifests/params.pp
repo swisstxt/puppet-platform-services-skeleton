1115a381f65371c14cafba6979517f6802d10b8c
Simon Josi <simon.josi@swisstxt.ch>
2013-06-11 13:58:30 +0200

    fix default params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,5 @@
-$puppetmaster = 'puppet-bie-12.bie.serv.poc.srf.mpc'
-$region = hiera('region')
-$project = hiera('project')
-$ue = hiera('ue')
+$domain_serv    = "${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc"
+$domain_stor    = "${::mpc_zone}.stor.${::mpc_project}.${::mpc_bu}.mpc"
+$domain_sync    = "${::mpc_zone}.sync.${::mpc_project}.${::mpc_bu}.mpc"
+
+$puppetmaster     = "puppet-${::mpc_zone}-01.${domain_serv}"
